ba29ab20963a97bee99dd5264f2f4760fd29d274
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2021-02-02 18:34:32 +0200

    style fixes/Home

--- src/js/history/updatePageHistory.js
+++ src/js/history/updatePageHistory.js
@@ -20,24 +20,14 @@ function fun1() {
   swiper1.start();
 }
 
-function fun2() {
-  root.innerHTML = renderCard(pages[2]);
-}
+function fun2() {}
 
-function fun3() {
-  root.innerHTML = renderCard(pages[3]);
-}
+function fun3() {}
 
-function fun4() {
-  root.innerHTML = renderCard(pages[4]);
-}
+function fun4() {}
 
-function fun5() {
-  root.innerHTML = renderCard(pages[5]);
-}
+function fun5() {}
 
-function fun6() {
-  root.innerHTML = renderCard(pages[1]);
-}
+function fun6() {}
 
 export { fun1, fun2, fun3, fun4, fun5, fun6 };
