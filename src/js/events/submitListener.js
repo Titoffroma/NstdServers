3c2952549ff43eda2378e86a9afa9b174ef71b48
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2021-02-25 09:34:10 +0200

    submit fix

--- src/js/events/submitListener.js
+++ src/js/events/submitListener.js
@@ -1,4 +1,17 @@
+import { lang } from '../history/mainHistory';
+import errorsLocal from '../localization/errors.json';
+
 export default function listenSubmit(event) {
   event.preventDefault();
-  console.dir(event);
+  const form = event.target.closest('form');
+  const errors = Array.from(form.querySelectorAll('.modal__message'));
+  const inputs = Array.from(form.querySelectorAll('.modal__input'));
+  const valid = inputs.filter(input => input.value);
+  const filter = errors.filter(el => !el.textContent.length);
+  if (valid.length === errors.length && filter.length === errors.length) {
+    const message = errorsLocal[lang.name].submit;
+    errors[0].classList.add('iserror');
+    form.elements[0].classList.add('invalid');
+    errors[0].textContent = message;
+  }
 }
