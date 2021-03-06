1198ce228ee07d9b0953e24de740cd5ab66119c1
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2021-03-06 02:55:55 +0200

    contacts animation

--- src/js/pages/contacts.js
+++ src/js/pages/contacts.js
@@ -1,4 +1,5 @@
-import img1 from '../../img/contacts-1.png';
+import img1 from '../../img/nebula.png';
+import img2 from '../../img/nebula1.png';
 import svg1 from '../../img/contact1.svg';
 import svg2 from '../../img/contact2.svg';
 import svg3 from '../../img/contact3.svg';
@@ -7,8 +8,10 @@ const svgs = [svg1, svg2, svg3];
 
 function drawContactsImages() {
   const image = document.querySelector('.contacts__image');
+  const image1 = document.querySelector('.contacts__image.abs');
   let fix = location.href.includes('github') ? 'NstdServers/' : '';
   image.src = fix + img1;
+  image1.src = fix + img2;
   const images = document.querySelectorAll('.contacts__center-image');
   images.forEach((svg, i) => {
     svg.src = svgs[i];
